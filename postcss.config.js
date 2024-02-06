40802b47991a6b9b601a83763c8f5830d730381c
alnavarrop99 <anavarro.develop99@gmail.com>
2024-02-06 18:20:03 -0500

    chore(dev): config postcss

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,12 @@ export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    "postcss-custom-properties": {},
+    "postcss-preset-env": {
+      autoprefixer: false,
+      features: {
+        "nesting-rules": true,
+      },
+    },
   },
-}
+};
