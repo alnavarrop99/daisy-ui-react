00bad0f29da2ad039bf78fcb2351a922a43ec2e9
alnavarrop99 <anavarro.develop99@gmail.com>
2024-02-07 15:22:20 -0500

    chore(dev): config storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,27 @@
+import type { StorybookConfig } from '@storybook/react-vite'
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
+  ],
+  viteFinal(config) {
+    return {
+      ...config,
+      css: {
+        postcss: './.storybook/postcssrs.json',
+      },
+    }
+  },
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+}
+export default config
