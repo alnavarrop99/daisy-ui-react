00e7645972941825b5ee047286cd57bc75a65d28
alnavarrop99 <anavarro.develop99@gmail.com>
2024-02-16 18:03:48 -0500

    chore: set alias @component @layout @assets @pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,19 @@ import legacy from '@vitejs/plugin-legacy'
 import progress from 'vite-plugin-progress'
 import svgr from 'vite-plugin-svgr'
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
+import { resolve } from 'path'
+
+const root = resolve(__dirname, 'src')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@component': resolve(root, 'component'),
+      '@layout': resolve(root, 'layout'),
+      '@pages': resolve(root, 'pages'),
+      '@assets': resolve(root, 'assets'),
+    },
+  },
   plugins: [react(), legacy({}), progress(), svgr(), TanStackRouterVite()],
 })
