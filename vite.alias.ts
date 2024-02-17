5c500179911b4db469b1722d5fe60a0f7a133939
alnavarrop99 <anavarro.develop99@gmail.com>
2024-02-17 01:54:02 -0500

    chore: create helper wrappers for storybook and rtl

--- vite.alias.ts
+++ vite.alias.ts
@@ -7,6 +7,7 @@ export const alias: Record<string, string> = {
   '@layout': resolve(root, 'layout'),
   '@pages': resolve(root, 'pages'),
   '@assets': resolve(root, 'assets'),
+  '@': resolve(root, 'helper.tsx'),
 }
 
 export default alias
