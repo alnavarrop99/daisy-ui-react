0710aebfb64df5a1eb02f2823fba549a7271d4f2
alnavarrop99 <anavarro.develop99@gmail.com>
2024-02-20 00:41:53 -0500

    feat: create @component login-form/ - file: storie/ - file: react/ - rest file: test/

---
+++ src/component/login-form.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { LoginForm } from '@component/login-form'
+
+const meta: Meta = {
+  title: '@component/login-form',
+  component: LoginForm,
+}
+export default meta
+type TStory = StoryObj<typeof LoginForm>
+
+export const Primary: TStory = {
+  args: {},
+}
---
+++ src/component/login-form.tsx
@@ -0,0 +1,41 @@
+import React, { useRef } from 'react'
+
+export function LoginForm() {
+  const ref = useRef<HTMLFormElement>(null)
+  const onSubmit: React.ReactEventHandler<HTMLFormElement> = (ev) => {
+    ev.preventDefault()
+    if (!ref.current) {
+      return
+    }
+    /* eslint-disable */
+    const { email, password, username } = Object.fromEntries(
+      new FormData(ref.current).entries()
+    ) as LoginFormEntry
+  }
+  return (
+    <div>
+      <form className="space-y-4 [&>*]:block" {...{ onSubmit, ref }}>
+        <label>
+          <span>username</span>
+          <input type="text" name="username" className="border" />
+        </label>
+        <label>
+          <span>email</span>
+          <input type="email" name="email" className="border" />
+        </label>
+        <label>
+          <span>password</span>
+          <input type="password" name="password" className="border" />
+        </label>
+        <button className="border" type="submit">
+          login
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default LoginForm
+
+type FormName = 'username' | 'password' | 'email'
+type LoginFormEntry = Record<FormName, FormDataEntryValue>
