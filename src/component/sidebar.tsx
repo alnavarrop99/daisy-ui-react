428417c2e05f36c14950763be5d05c94d7d501a7
alnavarrop99 <anavarro.develop99@gmail.com>
2024-02-22 15:10:00 -0500

    fix: update @component sidebar

--- src/component/sidebar.tsx
+++ src/component/sidebar.tsx
@@ -1,34 +1,110 @@
+import { $ } from '@'
 import { Link } from '@tanstack/react-router'
-import { useMemo } from 'react'
+import React, { useMemo } from 'react'
 
-export interface SideBarProp {
+/* eslint-disable-next-line */
+export interface TSideBarProp {
   list: Record<string, string>
 }
 
-export const SideBar = ({ list: _list }: SideBarProp) => {
+/* eslint-disable-next-line */
+export const SideBar = ({
+  children,
+  list: _list,
+  ...props
+}: React.PropsWithChildren<TSideBarProp> &
+  React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLUListElement>,
+    HTMLUListElement
+  >) => {
   const list = useMemo(() => Object.entries(_list), [])
   return (
-    <ul aria-label="nav group" className="flex flex-col space-y-4">
-      {list.map(([key, url]) => (
-        <li key={key} aria-label="nav link">
-          <Link to={url} className="group capitalize no-underline">
-            {({ isActive }) => (
-              <span
-                role="radio"
-                aria-checked={isActive}
-                aria-label="nav text and nav active"
-                className={[
-                  'hover:text-blue-500 group-active:underline',
-                  !isActive ? 'text-black' : 'text-blue-500',
-                ].join(' ')}
-              >
-                {key}
-              </span>
-            )}
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <nav
+      {...props}
+      aria-label="navbar"
+      className={$.clcs([
+        'inline-flex flex-col items-start justify-stretch gap-4 border border-black p-4',
+        props?.className ?? '',
+      ])}
+    >
+      {children}
+      <form>
+        <input className="border border-black" name="search" type="search" />
+      </form>
+      <ul
+        className="direction-inherit flex w-full justify-stretch gap-[inherit] px-4 [&>*_a]:flex [&>*_a]:justify-between"
+        aria-label="nav list"
+      >
+        {list.map(([key, url]) => (
+          <li key={key} aria-label="nav items">
+            <Link to={url} className="group capitalize no-underline">
+              {({ isActive }) => (
+                <>
+                  <span
+                    role="radio"
+                    aria-checked={isActive}
+                    aria-label="nav text and nav active"
+                    className={[
+                      $.clco({
+                        'text-blue-500': isActive,
+                        underline: isActive,
+                      }),
+                      'hover:text-blue-500',
+                    ].join(' ')}
+                  >
+                    {key}
+                  </span>
+                  {!isActive ? <span>+</span> : <span>-</span>}
+                </>
+              )}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
+
+/* eslint-disable-next-line */
+SideBar.Brand = function ({
+  children,
+  ...props
+}: React.PropsWithChildren &
+  React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLSpanElement>,
+    HTMLSpanElement
+  >) {
+  return (
+    <span
+      {...props}
+      aria-label="brand label"
+      className={$.clcs([props?.className ?? ''])}
+    >
+      {children}
+    </span>
+  )
+}
+
+/* eslint-disable-next-line */
+SideBar.Img = function ({
+  children,
+  'url-img': src,
+  ...props
+}: React.PropsWithChildren<{ 'url-img': string }> &
+  React.DetailedHTMLProps<
+    React.ImgHTMLAttributes<HTMLImageElement>,
+    HTMLImageElement
+  >) {
+  return (
+    <div aria-label="user image" className="flex flex-row items-center gap-2">
+      <img
+        {...props}
+        className={$.clcs(['rounded-full', props?.className ?? ''])}
+        alt="users"
+        src={src}
+      />
+      <span>{children}</span>
+    </div>
   )
 }
 
