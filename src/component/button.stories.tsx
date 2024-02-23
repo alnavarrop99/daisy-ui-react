1b9c3640163ff175a717bda861c5836ee1fee66a
alnavarrop99 <anavarro.develop99@gmail.com>
2024-02-22 23:08:22 -0500

    feat: create a button @compnonent

--- src/component/button.stories.tsx
+++ src/component/button.stories.tsx
@@ -1,146 +1,131 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import { Button, TButton } from "./button";
+import type { Meta, StoryObj } from '@storybook/react'
+import { Button, TButton } from './button'
+import Heart from '@/assets/heart.svg?react'
 
 function Basic({ label, ...props }: TButton & { label: string }) {
-  return <Button {...props}>{label}</Button>;
+  return <Button {...props}>{label}</Button>
 }
 
-function IconsStart({ label, ...props }: TButton & { label: string }) {
-  return (
-    <Button {...props}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="size-6"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-        />
-      </svg>
-      {label}
-    </Button>
-  );
-}
-
-function IconsEnd({ label, ...props }: TButton & { label: string }) {
-  return (
-    <Button {...props}>
-      {label}
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="size-6"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-        />
-      </svg>
-    </Button>
-  );
-}
-
-function Loading(position: "start" | "end") {
-  if (position === "start") {
+function Icons(position: 'start' | 'end') {
+  if (position === 'end') {
+    return function ({ label, ...props }: TButton & { label: string }) {
+      return (
+        <Button {...props}>
+          {label}
+          <Heart />
+        </Button>
+      )
+    }
+  }
+  return function ({ label, ...props }: TButton & { label: string }) {
     return (
-      { label, loading, loadingLabel, ...props }: TButton & {
-        label: string;
-        loadingLabel: string;
-        loading?: boolean;
-      },
-    ) => {
+      <Button {...props}>
+        <Heart />
+        {label}
+      </Button>
+    )
+  }
+}
+
+function Loading(position: 'start' | 'end') {
+  if (position === 'end') {
+    return ({
+      label,
+      loading,
+      loadingLabel,
+      ...props
+    }: TButton & {
+      label: string
+      loadingLabel: string
+      loading?: boolean
+    }) => {
       return (
         <Button {...props} loading={loading}>
           {loading ? loadingLabel : label}
           <Button.Loading />
         </Button>
-      );
-    };
-  } else {return (
-      { label, loading, loadingLabel, ...props }: TButton & {
-        label: string;
-        loadingLabel: string;
-        loading?: boolean;
-      },
-    ) => {
-      return (
-        <Button {...props} loading={loading}>
-          <Button.Loading />
-          {loading ? loadingLabel : label}
-        </Button>
-      );
-    };}
+      )
+    }
+  }
+  return ({
+    label,
+    loading,
+    loadingLabel,
+    ...props
+  }: TButton & {
+    label: string
+    loadingLabel: string
+    loading?: boolean
+  }) => {
+    return (
+      <Button {...props} loading={loading}>
+        <Button.Loading />
+        {loading ? loadingLabel : label}
+      </Button>
+    )
+  }
 }
 
 const meta: Meta = {
-  title: "@sys-design/button",
+  title: '@sys-design/button',
   component: Button,
-};
-export default meta;
+}
+export default meta
 
 const args: { label: string } = {
-  label: "Button",
-};
+  label: 'Button',
+}
 
 export const _Basic: StoryObj<TButton & { label: string }> = {
-  name: "Basic Property",
+  name: 'Basic Property',
   args,
   render: Basic,
-};
+}
 
 export const _IconsStart: StoryObj<TButton & { label: string }> = {
-  name: "Icon Start",
+  name: 'Icon Start',
   args,
-  render: IconsStart,
-};
+  render: Icons('start'),
+}
 
 export const _IconsEnd: StoryObj<TButton & { label: string }> = {
-  name: "Icon End",
+  name: 'Icon End',
   args,
-  render: IconsEnd,
-};
+  render: Icons('end'),
+}
 
 export const _LoadingStart: StoryObj<
   TButton & { label: string; loadingLabel: string; loading?: boolean }
 > = {
-  name: "Loading End",
+  name: 'Loading Start',
   args: {
     ...args,
     loading: true,
-    loadingLabel: "Loading",
+    loadingLabel: 'Loading',
   },
   argTypes: {
     loadingLabel: {
-      name: "loading-label",
-      type: "string",
+      name: 'loading-label',
+      type: 'string',
     },
   },
-  render: Loading("start"),
-};
+  render: Loading('start'),
+}
 
 export const _LoadingEnd: StoryObj<
   TButton & { label: string; loadingLabel: string; loading?: boolean }
 > = {
-  name: "Loading Start",
+  name: 'Loading End',
   args: {
     ...args,
     loading: true,
-    loadingLabel: "Loading",
+    loadingLabel: 'Loading',
   },
   argTypes: {
     loadingLabel: {
-      name: "loading-label",
-      type: "string",
+      name: 'loading-label',
+      type: 'string',
     },
   },
-  render: Loading("end"),
-};
+  render: Loading('end'),
+}
