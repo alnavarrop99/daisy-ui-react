257e648c2ffbf14927c922b3dc5e1f0b13e2a8f1
alnavarrop99 <anavarro.develop99@gmail.com>
2024-02-24 00:41:48 -0500

    fix: rm console log

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite'
 import { join } from 'path'
 
 const root = join(__dirname, '.storybook')
+
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
@@ -15,7 +16,6 @@ const config: StorybookConfig = {
       ...config.css,
       postcss: join(root, '.postcssrc'),
     }
-    console.log(config.css?.postcss)
     return config
   },
   framework: {
